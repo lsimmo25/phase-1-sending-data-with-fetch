a8457fd6d2fc8eec8d87a283d0a573d0c4fc3ed8
Lyle Simmons <lylesimmons125@gmail.com>
2024-02-04 21:47:40 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (object) {
+        console.log(object)
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+      });
+
+
+function submitData() {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": "Steve",
+            "email": "steve@steve.com"
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const newId = data.id;
+        document.body.innerHTML += `<p>New ID: ${newId}</p>`
+    })
+    .catch(error => {
+        let message = "Unauthorized Access"
+        document.body.innerHTML += `<p>${error.message}</p>`
+    })
+}
+
+// submitData();
+
+
